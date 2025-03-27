5f05a222632963a90352deaea06bf1db47487b69
Ohm-Rajpal <ohm_rajpal@berkeley.edu>
2025-03-26 17:44:12 -0700

    Successfully scraped job data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from "express"
+import axios from "axios"
 import { clerkMiddleware, clerkClient, requireAuth, getAuth } from '@clerk/express'
 import cors from "cors"
 
@@ -10,6 +11,28 @@ const port = 3000
 app.use(cors()); // communication with frontend
 app.use(clerkMiddleware())
 app.use(express.json()); // parse JSON
+
+// plan
+// 1. scrape the data in a json format: complete
+// 2. iterate over the json and store that in mongodb cluster
+// 3. send the data from the mongodb cluster to the frontend
+
+async function getRawData() {
+    const scrapedData = await axios.get('https://serpapi.com/search', {
+        params: {
+            engine: "google_jobs",
+            q: "aerospace+engineering+internships+summer+2025",
+            location: "United States",
+            gl: "us",
+            hl: "en",
+            api_key: process.env.SERPAPI_KEY
+        }, 
+    })
+
+    const parsedData = scrapedData.data["jobs_results"]
+    console.log(parsedData)
+}
+
 
 // hello world
 app.get('/', (req, res) => {
@@ -22,6 +45,7 @@ app.get('/protected', requireAuth(), (req, res) => {
   
 app.get('/test', (req, res) => {
     getRawData()
+    res.redirect('/')
 })
 
 app.listen(port, () => {
